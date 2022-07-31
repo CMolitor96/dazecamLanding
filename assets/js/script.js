16d8fda6580bc3f2a91f3aa59468027b19229a2e
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-07-30 20:05:49 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var hiddenHand = document.querySelector('#hiddenHand');
+var handButton = document.querySelector('#handButton');
+var body = document.getElementsByTagName('body');
+var audio = document.querySelector('#audio');
+var audiotest = document.querySelector('#sound');
+function playAudio() {
+    var audio = document.querySelector('#audio');
+    console.log('hello');
+    audio.play();
+}
+
+var id=null;
+function moveHand() {
+    var hiddenHand = document.querySelector('#hiddenHand');
+    hiddenHand.setAttribute('style', 'visibility: visible;');
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 6);
+    function frame() {
+        if (pos == 275) {
+            clearInterval(id);
+            playAudio();
+            var flash = document.querySelector('.flash');
+            flash.setAttribute('style', 'visibility: visible;');
+            window.location.href = "https://apps.apple.com/us/app/daze-cam/id1464359734?itsct=apps_box_link&itscg=30200";
+        } else {
+            pos++;
+            hiddenHand.style.top = pos + 'px';
+            
+        }
+    }
+}
+
+handButton.addEventListener('click', moveHand);
